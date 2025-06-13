dcfb0b88e1a808b2bf942b7239f7e361c15e2e42
spacecentre <akkilalagar05@gmail.com>
2025-06-13 04:38:01 +0000

    setup font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import { Red_Hat_Mono } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "AWS Prep Help",
   description: "AWS Prep Help, Free practice exams for AWS certifications",
 };
+
+const redHatMono = Red_Hat_Mono({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-red-hat-mono",
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body>
+      <body className={`${redHatMono.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
